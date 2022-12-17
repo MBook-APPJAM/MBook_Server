49d5d4fb4111a1a19824ef23b5d5725a82f1a75c
beargame123 <hoyoung7827@gmail.com>
2022-12-17 20:57:49 +0900

    ➕:: passwordRequest add

---
+++ src/main/java/com/example/mbook/domain/user/dto/PasswordRequest.java
@@ -0,0 +1,16 @@
+package com.example.mbook.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Pattern;
+
+@Getter
+@AllArgsConstructor
+public class PasswordRequest {
+    private String originalPassword;
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    private String newPassword;
+    private String newPasswordValid;
+}
