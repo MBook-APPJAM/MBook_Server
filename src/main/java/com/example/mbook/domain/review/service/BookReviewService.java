dc927ac5b68df395f08a59ba8ff6ba08568c7297
beargame123 <hoyoung7827@gmail.com>
2022-12-18 01:40:18 +0900

    .

---
+++ src/main/java/com/example/mbook/domain/review/service/BookReviewService.java
@@ -0,0 +1,17 @@
+package com.example.mbook.domain.review.service;
+
+
+import com.example.mbook.domain.review.dto.BookReviewLists;
+import com.example.mbook.domain.review.dto.ReviewRequest;
+
+public interface BookReviewService {
+    void createReview(ReviewRequest request, Long id);
+
+    void delReview(Long id);
+
+    void setReview(Long id, ReviewRequest request);
+
+    BookReviewLists myBookReview();
+
+    BookReviewLists otherBookReview(Long id);
+}
