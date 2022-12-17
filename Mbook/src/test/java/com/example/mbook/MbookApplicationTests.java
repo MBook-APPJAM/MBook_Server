36108ef02fa6110499cb889b88b0491c3a6a6ae2
김호영 <81502235+beargame123@users.noreply.github.com>
2022-12-17 15:45:42 +0900

    ⚙️:: setting

---
+++ Mbook/src/test/java/com/example/mbook/MbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
