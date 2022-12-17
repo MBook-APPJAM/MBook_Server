7179beecce2b72db8f03f69363ecded5e0652f9a
beargame123 <hoyoung7827@gmail.com>
2022-12-17 17:22:02 +0900

    ➕:: User Facade Pattern add

---
+++ src/main/java/com/example/mbook/domain/user/facade/UserFacade.java
@@ -0,0 +1,35 @@
+package com.example.mbook.domain.user.facade;
+
+import com.example.mbook.domain.user.entity.User;
+import com.example.mbook.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUserByAccountId(accountId);
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+
+    }
+
+    public User getUserByAccountId(String nickName) {
+        return userRepository.findUserByNickName(nickName)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("이메일을 찾을 수 없습니다."));
+    }
+}
