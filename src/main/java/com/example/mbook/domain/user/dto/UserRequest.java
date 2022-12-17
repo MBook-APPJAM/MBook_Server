f1a9db8c1f0f11eb1af21a0dee75fd20f74fd6d5
beargame123 <hoyoung7827@gmail.com>
2022-12-17 20:57:24 +0900

    ➕:: userRequest Dto add

---
+++ src/main/java/com/example/mbook/domain/user/dto/UserRequest.java
@@ -0,0 +1,10 @@
+package com.example.mbook.domain.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserRequest {
+    private String nickName;
+}
