b310d408ce0cac22e901d9b6ee15336791f22578
beargame123 <hoyoung7827@gmail.com>
2022-12-18 01:39:52 +0900

    .

---
+++ src/main/java/com/example/mbook/domain/review/dto/MovieReviewList.java
@@ -0,0 +1,10 @@
+package com.example.mbook.domain.review.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MovieReviewList {
+    private final String title;
+}
