1caa5be5df3de045a943c7d1acc6b3572e609089
beargame123 <hoyoung7827@gmail.com>
2022-12-18 00:34:35 +0900

    .

---
+++ src/main/java/com/example/mbook/domain/movie/entity/Movie.java
@@ -0,0 +1,50 @@
+package com.example.mbook.domain.movie.entity;
+
+import com.example.mbook.domain.feed.entity.MovieLove;
+import com.example.mbook.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String imageUrl;
+
+    private String localDate;
+
+    private String introduce;
+
+    private String category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "movie", cascade = CascadeType.REMOVE)
+    private List<MovieLove> movieLoves;
+
+    @Builder
+    public Movie(String title, String content, String imageUrl, String localDate, String introduce, User user, String category){
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.localDate = localDate;
+        this.introduce = introduce;
+        this.user = user;
+        this.category = category;
+    }
+}
