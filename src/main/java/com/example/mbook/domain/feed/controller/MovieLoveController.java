07943efe499a55917e366f777d0066f97080033a
beargame123 <hoyoung7827@gmail.com>
2022-12-18 00:33:38 +0900

    .

---
+++ src/main/java/com/example/mbook/domain/feed/controller/MovieLoveController.java
@@ -0,0 +1,30 @@
+package com.example.mbook.domain.feed.controller;
+
+import com.example.mbook.domain.feed.service.MovieLoveService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "movie_love", description = "영화 북마크 API 입니다.")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/movie")
+public class MovieLoveController {
+    private final MovieLoveService movieLoveService;
+
+    @Operation(description = "북마크 누르기")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping("/like/{movieId}")
+    public void wishPost(@PathVariable(name = "movieId") Long movieId) {
+        movieLoveService.movieLike(movieId);
+    }
+
+    @Operation(description = "북마크 취소")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/like/cancel/{bookId}")
+    public void wishCancel(@PathVariable(name = "movieId") Long movieId) {
+        movieLoveService.movieLikeCancel(movieId);
+    }
+}
