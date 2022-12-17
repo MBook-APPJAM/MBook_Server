9215cebd97bb0f78313e3a8b364ee928bd263e6a
beargame123 <hoyoung7827@gmail.com>
2022-12-18 00:33:13 +0900

    .

---
+++ src/main/java/com/example/mbook/domain/book/dto/BookRequest.java
@@ -0,0 +1,15 @@
+package com.example.mbook.domain.book.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BookRequest {
+    private final String title;
+    private final String content;
+    private final String imageUrl;
+    private final String localDate;
+    private final String introduce;
+    private final String category;
+}
