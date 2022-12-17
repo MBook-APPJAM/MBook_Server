03a6bead7dc9d188fbe0edbd89ee20c33eca5f67
beargame123 <hoyoung7827@gmail.com>
2022-12-17 17:19:17 +0900

    ➕:: jwtProvider add

---
+++ src/main/java/com/example/mbook/global/secuirty/jwt/JwtProvider.java
@@ -0,0 +1,111 @@
+package com.example.mbook.global.secuirty.jwt;
+
+import com.example.mbook.domain.user.dto.UserResponse;
+import com.example.mbook.global.secuirty.jwt.dto.TokenResponse;
+import com.example.mbook.global.secuirty.jwt.entity.RefreshToken;
+import com.example.mbook.global.secuirty.jwt.repository.RefreshTokenRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Value("${spring.jwt.key}")
+    private String key;
+
+    @Value("${spring.jwt.live.atk}")
+    private Long atkTime;
+
+    @Value("${spring.jwt.live.rtk}")
+    private Long rtkTime;
+
+    @PostConstruct
+    protected void init() {
+        key = Base64.getEncoder().encodeToString(key.getBytes());
+    }
+
+    public TokenResponse createTokenByLogin(UserResponse userResponse) {
+        String atk = createToken(userResponse, atkTime, "atk");
+        String rtk = createToken(userResponse, rtkTime, "rtk");
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .email(userResponse.getEmail())
+                        .rtk(rtk)
+                        .rtkTime(rtkTime).build());
+        return new TokenResponse(atk, rtk);
+    }
+
+    // Jwt 토큰 인증 정보조회
+    public Authentication getAuthentication(String token) {
+        // Jwt 에서 정보 추출
+        Claims claims = parseClaims(token);
+        // 권한 정보가 없음
+        if (claims.get("roles") == null) {
+            throw new IllegalStateException("Role Not Found Exception");
+        }
+        UserDetails userDetails = userDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // Jwt 토큰 복호화해서 가져오기
+    public Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    // Request의 Header 에서 token값을 가져옴. "Authorization" : "TOKEN 값"
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+
+    // 토큰의 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            throw new IllegalStateException("Token 만료");
+        } catch (Exception e) {
+            throw new IllegalStateException("Token 만료");
+        }
+    }
+
+    public TokenResponse reissueAtk(UserResponse userResponse) {
+        boolean rtkInRedis = refreshTokenRepository.existsByEmail(userResponse.getEmail());
+        if (rtkInRedis) throw new IllegalStateException("Token 만료");
+        return new TokenResponse(createToken(userResponse, atkTime, "atk"), null);
+    }
+
+    private String createToken(UserResponse userResponse, Long tokenTime, String type) {
+        Claims claims = Jwts.claims().setSubject(userResponse.getEmail());
+        claims.put("roles", userResponse.getRole());
+        Date date = new Date();
+        return Jwts.builder()
+                .claim("type", type)
+                .setClaims(claims)
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + tokenTime))
+                .signWith(SignatureAlgorithm.HS256, key).compact();
+    }
+}
